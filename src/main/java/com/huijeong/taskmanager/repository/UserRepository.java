fdddf1c1be53a7c8d000debfa23af94993a40e41
0237hh <153145663+0237hh@users.noreply.github.com>
2025-02-04 22:55:40 +0900

    user 리포지토리 작성

--- src/main/java/com/huijeong/taskmanager/repository/UserRepository.java
+++ src/main/java/com/huijeong/taskmanager/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.huijeong.taskmanager.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.huijeong.taskmanager.entity.Users;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users,Integer> {
+    Optional<Users> findByUserEmail(String username);
 }
